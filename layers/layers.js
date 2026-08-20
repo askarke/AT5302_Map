af8879645e3bfaa763f6603453645b4fe02d53c3
askarke <89739649+askarke@users.noreply.github.com>
2026-08-19 21:08:23 -0500

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -3,6 +3,7 @@ var wms_layers = [];
 
         var lyr_ESRIOcean_0 = new ol.layer.Tile({
             'title': 'ESRI Ocean',
+            'type':'base',
             'opacity': 1.000000,
             
             
@@ -25,6 +26,27 @@ var lyr_CindysSiteFigExtracted_1 = new ol.layer.Image({
             imageExtent: [-8291186.993774, 4486198.921671, -8205693.624845, 4588852.575555]
         })
     });
+var format_Skarkeetal2024_USAMseeps_duplicatesremoved_2 = new ol.format.GeoJSON();
+var features_Skarkeetal2024_USAMseeps_duplicatesremoved_2 = format_Skarkeetal2024_USAMseeps_duplicatesremoved_2.readFeatures(json_Skarkeetal2024_USAMseeps_duplicatesremoved_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Skarkeetal2024_USAMseeps_duplicatesremoved_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Skarkeetal2024_USAMseeps_duplicatesremoved_2.addFeatures(features_Skarkeetal2024_USAMseeps_duplicatesremoved_2);
+var lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Skarkeetal2024_USAMseeps_duplicatesremoved_2, 
+                style: style_Skarkeetal2024_USAMseeps_duplicatesremoved_2,
+                popuplayertitle: 'Skarkeetal2024_USAMseeps_duplicatesremoved',
+                interactive: true,
+                title: '<img src="styles/legend/Skarkeetal2024_USAMseeps_duplicatesremoved_2.png" /> Skarkeetal2024_USAMseeps_duplicatesremoved'
+            });
 
-lyr_ESRIOcean_0.setVisible(true);lyr_CindysSiteFigExtracted_1.setVisible(true);
-var layersList = [lyr_ESRIOcean_0,lyr_CindysSiteFigExtracted_1];
+lyr_ESRIOcean_0.setVisible(true);lyr_CindysSiteFigExtracted_1.setVisible(true);lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2.setVisible(true);
+var layersList = [lyr_ESRIOcean_0,lyr_CindysSiteFigExtracted_1,lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2];
+lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2.set('fieldAliases', {'field_1': 'field_1', 'field_2': 'field_2', 'field_3': 'field_3', 'field_4': 'field_4', 'field_5': 'field_5', 'field_6': 'field_6', 'field_7': 'field_7', 'field_8': 'field_8', 'field_9': 'field_9', });
+lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2.set('fieldImages', {'field_1': '', 'field_2': '', 'field_3': '', 'field_4': '', 'field_5': '', 'field_6': '', 'field_7': '', 'field_8': '', 'field_9': '', });
+lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2.set('fieldLabels', {'field_1': 'no label', 'field_2': 'no label', 'field_3': 'no label', 'field_4': 'no label', 'field_5': 'no label', 'field_6': 'no label', 'field_7': 'no label', 'field_8': 'no label', 'field_9': 'no label', });
+lyr_Skarkeetal2024_USAMseeps_duplicatesremoved_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
